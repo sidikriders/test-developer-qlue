f4cecf4a70315563d9aa5fd2b756ea07f7e66e39
sidikriders <your@example.com>
2017-08-24 14:58:04 +0700

    tambah transisi munculin navBar

--- client/src/components/MapsLeaflet.js
+++ client/src/components/MapsLeaflet.js
@@ -12,13 +12,30 @@ class MapsLeaflet extends React.Component {
       centerView: [51.505, -0.09],
       markerIcon: icon({
         iconUrl: 'http://www.qlue.co.id/vacancy/svc/icon-marker.png'
-      })
+      }),
+      showNavigation: false,
+      style: {
+        navBar: {
+          width: '5%',
+          height: '100vh',
+          borderLeft: 'black 1px solid',
+          overflowY: "scroll",
+          paddingLeft: "20px",
+          transition: "width 1s"
+        },
+        mapNya: {
+          width: '95%',
+          height: '100vh',
+          borderRight: 'black 2px solid',
+          transition: "width 1s"
+        }
+      }
     }
   }
   render() {
     return (
       <div style={{display: "flex"}}>
-        <Map center={this.state.centerView} zoom={13} style={{width: '50%', height: '100vh', borderRight: 'black 2px solid'}}>
+        <Map center={this.state.centerView} zoom={13} style={this.state.style.mapNya}>
           {
             this.props.markerList.length > 0 ?
             <div>
@@ -45,24 +62,36 @@ class MapsLeaflet extends React.Component {
             null
           }
         </Map>
-        <div id="navigasinya" style={{width: '50%', height: '100vh', borderLeft: 'black 1px solid', overflowY: "scroll", paddingLeft: "20px"}}>
+        <div id="navigasinya" style={this.state.style.navBar}>
           {
             this.props.markerList.length > 0 ?
             <div>
-              <h2><strong>Marker Navigation</strong></h2>
-              <div style={{textAlign: "left"}}>
-                {
-                  this.props.markerList.map( (x) => {
-                    let lat = x.lat;
-                    let lng = x.lng;
-                    return (
-                      <div key={x.placemark_id} style={{marginBottom: "20px"}}>
-                        <span> {x.name} </span><button style={{marginLeft: "20px"}} onClick={ () => this.changeMapView(lat, lng)}>Look!</button>
-                      </div>
-                    )
-                  })
-                }
+              <div style={{textAlign: "right", paddingRight: "10px", paddingTop: "10px"}}>
+                <button onClick={() => this.showNavBar()}>
+                  <img style={{ height: "30px", width: "30px" }} src="https://cdn4.iconfinder.com/data/icons/flat-black/128/menu.png"/>
+                </button>
               </div>
+              {
+                this.state.showNavigation ?
+                <div>
+                  <h2><strong>Marker Navigation</strong></h2>
+                  <div style={{textAlign: "left"}}>
+                    {
+                      this.props.markerList.map( (x) => {
+                        let lat = x.lat;
+                        let lng = x.lng;
+                        return (
+                          <div key={x.placemark_id} style={{marginBottom: "20px"}}>
+                            <span> {x.name} </span><button style={{marginLeft: "20px"}} onClick={ () => this.changeMapView(lat, lng)}>Look!</button>
+                          </div>
+                        )
+                      })
+                    }
+                  </div>
+                </div>
+                :
+                null
+              }
             </div>
             :
             null
@@ -70,6 +99,20 @@ class MapsLeaflet extends React.Component {
         </div>
       </div>
     )
+  }
+
+  showNavBar() {
+    if (this.state.showNavigation) {
+      this.setState({
+        style: {...this.state.style, navBar: {...this.state.style.navBar, width: "5%"}, mapNya: {...this.state.style.mapNya, width: "95%"}},
+        showNavigation: false
+      })
+    } else {
+      this.setState({
+        style: {...this.state.style, navBar: {...this.state.style.navBar, width: "50%"}, mapNya: {...this.state.style.mapNya, width: "50%"}},
+        showNavigation: true
+      })
+    }
   }
 
   changeMapView(lat, lng) {
